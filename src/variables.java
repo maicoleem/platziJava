0eb33b29fc4d465e588aacc7933a227dacd2a1a0
maicolPCprincipal <maicoleem@gmail.com>
2022-12-14 18:33:54 -0500

    actualizando variables, se usa += para agregar valores auna variable

--- src/variables.java
+++ src/variables.java
@@ -4,5 +4,15 @@ public class variables {
         int speed;
         speed = 10;
         System.out.println(speed);
+        speed = speed + 20;
+        System.out.println(speed);
+        speed += 40;
+        System.out.println(speed);
+
+        //variable tipo texto
+        String employedName = "Maicol";
+        System.out.println(employedName);
+        employedName += " Escobar";
+        System.out.println(employedName);
     }
 }
