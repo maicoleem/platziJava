028bb08e13d0ee94ec4ac6b8ff19519f62f23dbb
maicolPCprincipal <maicoleem@gmail.com>
2022-12-14 18:24:57 -0500

    Clase de variables 11, creando variables; el proyecto solo debe de tener un método main (quí solo por este clase).

---
+++ src/variables.java
@@ -0,0 +1,8 @@
+public class variables {
+    public static void main(String[] args){
+        //Declarando variables
+        int speed;
+        speed = 10;
+        System.out.println(speed);
+    }
+}
