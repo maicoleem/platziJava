2a3e5c922ea3942d7352cd017b7756a46dec025a
maicolPCprincipal <maicoleem@gmail.com>
2022-12-24 12:52:40 -0500

    Clase 8: implementanto metodos estaticos y variables estaticas en doctor, static id incrementa sin el static siempre se iniciaria en 0.

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,21 +1,27 @@
 public class Doctor {
     //atributos
-    int id;
+    static int id = 0;
     String name;
     String speciality;
 
     //metodo constructor, no es necesario sentenciarlo (escribirlo)
     Doctor(){
         //se puede añadir el comportamiento pero no es necesario por ahora.
+        Doctor.id++;
     }
     //se puede tener dos opciones de construcción
     Doctor(String name){
         System.out.println("El nombre del doctor es: " + name);
+        Doctor.id++;
     }
 
     //comportamientos
     public void showName(){
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("El ID del Doctor es: " + id);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
+import javax.print.Doc;
+
+import static UI.UImenu.*;
 public class Main {
     public static void main(String[] args) {
         Doctor firstDoctor = new Doctor();
-        Doctor secondDoctor = new Doctor();
-        Doctor thirdDoctor = new Doctor("Eskorbuto");
         firstDoctor.name = "Maicol";
+        firstDoctor.showId();
+
+
+        Doctor secondDoctor = new Doctor();
         secondDoctor.name = "Daniela";
+        secondDoctor.showId();
+
+        //Doctor thirdDoctor = new Doctor("Eskorbuto");
+
         firstDoctor.showName();
         secondDoctor.showName();
+
+        //showMenu();
+
     }
 }
