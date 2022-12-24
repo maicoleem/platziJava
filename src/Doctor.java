da2eda2706b836f641692180b34ff251ed7b2b5f
maicolPCprincipal <maicoleem@gmail.com>
2022-12-24 11:12:15 -0500

    Clase 6: Utilizando el método constructor.

--- src/Doctor.java
+++ src/Doctor.java
@@ -4,6 +4,15 @@ public class Doctor {
     String name;
     String speciality;
 
+    //metodo constructor, no es necesario sentenciarlo (escribirlo)
+    Doctor(){
+        //se puede añadir el comportamiento pero no es necesario por ahora.
+    }
+    //se puede tener dos opciones de construcción
+    Doctor(String name){
+        System.out.println("El nombre del doctor es: " + name);
+    }
+
     //comportamientos
     public void showName(){
         System.out.println(name);
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Doctor firstDoctor = new Doctor();
         Doctor secondDoctor = new Doctor();
+        Doctor thirdDoctor = new Doctor("Eskorbuto");
         firstDoctor.name = "Maicol";
         secondDoctor.name = "Daniela";
         firstDoctor.showName();
